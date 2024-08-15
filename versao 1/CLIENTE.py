1535ff526f29516edddfce8ae9b969091a12a8cd
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-08-15 13:40:04 -0300

    cliente consegue mandar mensagens para o servidor

--- versao 1/CLIENTE.py
+++ versao 1/CLIENTE.py
@@ -1,14 +1,14 @@
 import socket
-import time
+
+# criar e abrir conexao
 host = '127.0.0.1'     # Endereco IP do Servidor
 porta = 5000           # Porta que o Servidor esta
 soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 destino = (host, porta)
 soquete.connect(destino)
-soquete.send("ola mundo")
 
-print("Aguardando para fechar conexao...")
-time.sleep(5)
-print("Fim da conexao")
+# enviar o nome do cliente para o servidor 
+nome = raw_input("Informe seu nome: ")
+soquete.send(nome.encode('utf-8'))
 
 soquete.close()
--- versao 1/SERVIDOR.py
+++ versao 1/SERVIDOR.py
@@ -1,21 +1,18 @@
 import socket
 import threading
-import time
+
+lista_de_mensagens = []
 
 def atenderClientesSimultaneos(conexao, cliente):
-    print("Conectado por",  cliente)
-
-    # processando a mensagem
-    time.sleep(5)
-
-    mensagem = conexao.recv(1024)
-    print("Cliente ", cliente[0], "; Mensagem recebida: ", mensagem)  
-    conexao.close()
-    print("Cliente ", cliente[0], " saiu")
+    
+    # receber o nome do cliente
+    nome = conexao.recv(1024).decode("utf-8")
+    print "Conectado por:",  nome , cliente
+            
 
 host = '127.0.0.1'  # endereco IP do Servidor
 porta = 5000        # porta que o servidor esta
-soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # definir conexao com TCP
 origem = (host, porta)
 soquete.bind(origem)
 soquete.listen(0) 
