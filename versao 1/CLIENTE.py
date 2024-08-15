615cce1702d7bfc33505663b9ace2b9d0ac21bfc
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-08-15 13:13:15 -0300

    servidor multi-thread para atender mais de um cliente por vez

--- versao 1/CLIENTE.py
+++ versao 1/CLIENTE.py
@@ -1,9 +1,14 @@
 import socket
+import time
 host = '127.0.0.1'     # Endereco IP do Servidor
 porta = 5000           # Porta que o Servidor esta
 soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 destino = (host, porta)
 soquete.connect(destino)
-soquete.send("Ola Mundo!")
-soquete.close()
+soquete.send("ola mundo")
 
+print("Aguardando para fechar conexao...")
+time.sleep(5)
+print("Fim da conexao")
+
+soquete.close()
--- versao 1/SERVIDOR.py
+++ versao 1/SERVIDOR.py
@@ -1,12 +1,31 @@
-host = '127.0.0.1' # Endereco IP do Servidor
-porta = 5000       # Porta que o Servidor esta
+import socket
+import threading
+import time
+
+def atenderClientesSimultaneos(conexao, cliente):
+    print("Conectado por",  cliente)
+
+    # processando a mensagem
+    time.sleep(5)
+
+    mensagem = conexao.recv(1024)
+    print("Cliente ", cliente[0], "; Mensagem recebida: ", mensagem)  
+    conexao.close()
+    print("Cliente ", cliente[0], " saiu")
+
+host = '127.0.0.1'  # endereco IP do Servidor
+porta = 5000        # porta que o servidor esta
 soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 origem = (host, porta)
 soquete.bind(origem)
-soquete.listen(0)
+soquete.listen(0) 
+
 while True:
     conexao, cliente = soquete.accept()
-    print ('Conectado por', cliente)
-    mensagem = conexao.recv(1024)
-    print ("Cliente ", cliente[0], "Recebida: ", mensagem)
-    conexao.close()
+
+    try:
+        # criando contrutor Thread com a funcao para atender clientes simultaneos e a tupla com conexao e cliente
+        thread = threading.Thread(target=atenderClientesSimultaneos, args=(conexao, cliente)) # o que tiver no target usa o run
+        thread.start()
+    except:
+        print("Erro na conexao!")
