17ab003a2582f1a7f7a4d084916c0a7eff37e2b6
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-08-16 09:58:42 -0300

    thread que escuta outros clientes e não trava a interação do menu

--- versao 2/CLIENTE.py
+++ versao 2/CLIENTE.py
@@ -1,29 +1,36 @@
 import socket
 import threading
 
+def escutarOutrosClientes(conexao):
+     while True:
+        try:
+            mensagem, ip = conexao.recvfrom(1024)
+            print("\nMensagem recebida de", ip, ":", mensagem.decode())
+        except Exception as e:
+            break
+        
+
 # criar e abrir conexao com servidor
-host = '127.0.0.1'     
+host_servidor = '127.0.0.1'     
 porta_servidor = 5000  # porta que o Servidor esta
 soquete_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-destino_servidor = (host, porta_servidor)
+destino_servidor = (host_servidor, porta_servidor)
 soquete_servidor.connect(destino_servidor)
 
 # criar e abrir conexao com outro clientes
+host_cliente = '127.0.0.1'  
 porta_cliente = 50001 
 soquete_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-origem = (host, porta_cliente)
+origem = (host_cliente, porta_cliente)
 soquete_cliente.bind(origem)
 soquete_cliente.listen(0) 
 
+thread_escutar_clientes = threading.Thread(target=escutarOutrosClientes, args=(soquete_cliente,))
+thread_escutar_clientes.daemon = True # thread sera encerrada quando o programa principal terminar
+thread_escutar_clientes.start()
+
 endereco_cliente = {} # ip de clientes que ja mandou mensagem antes
 lista_mensagens_recebidas = [] # cliente vai armazenar suas proprias mensagens
-
-def escutarConexaoComOutrosClientes(conexao, cliente):
-     while True:
-        mensagem = conexao.recv(1024).decode('utf-8')
-        print("Mensagem recebida de",  cliente[0], ":",  mensagem)
-        lista_mensagens_recebidas.append((cliente[0], mensagem))
-
 
 # enviar o nome do cliente para o servidor 
 nome = raw_input("Informe seu nome: ")
@@ -31,12 +38,6 @@ soquete_servidor.send(nome.encode('utf-8'))
 
 # loop do menu de opcoes
 while True:
-    #conexao, cliente = soquete_cliente.accept()
-
-    #thread = threading.Thread(target=escutarConexaoComOutrosClientes, args=(conexao, cliente)) # o que tiver no target usa o run
-    #thread.start()
-
-
     # interacao entre cliente e servidor    
     menu = "\n................. Menu .................\n\nEscolha uma das opcoes\n1. Enviar mensagem para outro cliente\n2. Listar mensages recebidas\n3. Listar IP de clientes conhecidos\n4.cListar clientes conectados\n5. Sair"
     print(menu)
@@ -92,6 +93,5 @@ while True:
         
     else:
         print("Opcao invalida")
-
 
 soquete_cliente.close()
